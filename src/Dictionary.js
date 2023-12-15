8321ea8ae5e50f6e14b3db3119fc722da36bb7c8
meli-mac <melissamachado015@gmail.com>
2023-12-15 11:30:10 -0500

    Added Dictionary API
    - Added Axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching the definition for "${keyword}"`);
+    let apiKey = "560eatef1f75aofbc96fd19f393b84ab";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
