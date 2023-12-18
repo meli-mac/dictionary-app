7f1ea4daf6c97bbec26b86330be29d9ba6042ac1
meli-mac <melissamachado015@gmail.com>
2023-12-18 08:13:38 -0500

    Corrected error in Meanings.js

--- src/Meanings.js
+++ src/Meanings.js
@@ -3,18 +3,12 @@ import React from "react";
 export default function Meanings(props) {
   return (
     <div className="Meanings">
-      <h3>{props.meanings[0].partOfSpeech}</h3>
-      {props.meanings.definition.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
+      <h3>{props.meanings.partOfSpeech}</h3>
+      <p>
+        {props.meanings.definition}
+        <br />
+        <em>{props.meanings.example}</em>
+      </p>
     </div>
   );
 }
